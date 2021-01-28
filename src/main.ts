43f61e1a4e06ab15fdb3626a5575a31f733ea31f
mo5mami <mami.mokhtar123@gmail.com>
2021-01-28 08:53:26 +0100

    cors already done my bad

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({transform: true}));
-  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
